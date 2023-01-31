f095bed25c18ae06c27aea8e8351ffe52757d77a
macbook <frqhero@gmail.com>
2023-01-31 22:59:32 +0600

    me page allegedly works correctly regarding permissions and the overall result as well

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -140,6 +140,9 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.TokenAuthentication",
     ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 100,
 }
@@ -152,5 +155,6 @@ DJOSER = {
     },
     "SERIALIZERS": {
         "user_create": "users.serializers.MyDjoserUserCreateSerializer",
+        "user": "users.serializers.MyDjoserUserCreateSerializer",
     }
 }
--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,19 +1,12 @@
-import djoser.views
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, MyViewSet
-from djoser import views
+from .views import MyViewSet
 
-
-v1_router = DefaultRouter()
-v1_router.register('users', UserViewSet)
 
 urlpatterns = [
-    # path('', include(v1_router.urls)),
     path('auth/', include('djoser.urls.authtoken')),
     path('users/', MyViewSet.as_view({'get': 'list', 'post': 'create'})),
+    path('users/<int:id>/', MyViewSet.as_view({'get': 'retrieve'})),
+    path('users/me/', MyViewSet.as_view({'get': 'me'})),
     path('djoser/', include('djoser.urls')),
     path("heck/", include("django.contrib.auth.urls")),
-    # re_path(r'^auth/', include('djoser.urls.authtoken')),
 ]
-x = 1
