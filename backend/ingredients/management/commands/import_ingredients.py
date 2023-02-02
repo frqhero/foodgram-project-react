3e23cf5365d59605103a181f52dc62c21c45a6d9
macbook <frqhero@gmail.com>
2023-02-02 20:49:01 +0600

    management command for ingredients created

---
+++ backend/ingredients/management/commands/import_ingredients.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand
+import json
+from django.conf import settings
+from ingredients.models import Ingredient
+
+
+class Command(BaseCommand):
+    def fetch_data(self, filename):
+        django_root = settings.BASE_DIR
+        with open(f"{django_root}/data/{filename}", "r", encoding="utf-8-sig") as f:
+            res = f.read().replace("\n", "")
+        return json.loads(res)
+
+    def handle(self, *args, **options):
+        incoming_data = self.fetch_data("ingredients.json")
+        bulk = [Ingredient(**x) for x in incoming_data]
+        Ingredient.objects.bulk_create(bulk)
