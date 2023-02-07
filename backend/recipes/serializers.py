344d2ec282df0f3820cda79ee3e8ca63c1a4a1d4
macbook <frqhero@gmail.com>
2023-02-08 01:46:44 +0600

    recipe create serializer accept tags info, author creates and representation returned by read serializer

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -3,19 +3,21 @@ from rest_framework import serializers
 from .models import Recipe, RecipeIngredient
 from users.serializers import MyDjoserUserCreateSerializer
 from tags.serializers import TagSerializer
+from tags.models import Tag
 
 
 class RecipeIngredientSerializer(ModelSerializer):
     id = serializers.IntegerField(source='ingredient.id')
-    name = serializers.CharField (source='ingredient.name')
+    name = serializers.CharField(source='ingredient.name')
     measurement_unit = serializers.CharField(source='ingredient.measurement_unit')
+    amount = serializers.IntegerField(source='quantity')
 
     class Meta:
         model = RecipeIngredient
-        fields = ['id', 'name', 'measurement_unit', 'quantity']
+        fields = ['id', 'name', 'measurement_unit', 'amount']
 
 
-class RecipeSerializer(ModelSerializer):
+class RecipeReadSerializer(ModelSerializer):
     author = MyDjoserUserCreateSerializer()
     tags = TagSerializer(many=True)
     ingredients = RecipeIngredientSerializer(many=True, source='recipeingredient_set')
@@ -23,3 +25,16 @@ class RecipeSerializer(ModelSerializer):
     class Meta:
         model = Recipe
         fields = ['id', 'tags', 'author', 'ingredients', 'name', 'image', 'text', 'cooking_time']
+
+
+class RecipeCreateSerializer(ModelSerializer):
+    tags = serializers.PrimaryKeyRelatedField(
+        many=True, read_only=False, queryset=Tag.objects.all()
+    )
+
+    class Meta:
+        model = Recipe
+        fields = ['ingredients', 'tags', 'image', 'name', 'text', 'cooking_time']
+
+    def to_representation(self, instance):
+        return RecipeReadSerializer().to_representation(instance)
--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,15 +1,30 @@
 from rest_framework.viewsets import ModelViewSet
 from .models import Recipe
-from rest_framework.permissions import AllowAny
-from .serializers import RecipeSerializer
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from .serializers import RecipeReadSerializer, RecipeCreateSerializer
 from users.paginations import CustomPageNumberPagination
 from .filters import RecipeFilter
 from django_filters.rest_framework import DjangoFilterBackend
 
+
 class RecipeViewSet(ModelViewSet):
     queryset = Recipe.objects.all()
-    permission_classes = (AllowAny,)
-    serializer_class = RecipeSerializer
     pagination_class = CustomPageNumberPagination
     filter_backends = (DjangoFilterBackend,)
     filterset_class = RecipeFilter
+
+    def get_serializer_class(self):
+        if self.action == 'list' or self.action == 'retrieve':
+            return RecipeReadSerializer
+        elif self.action == 'create':
+            return RecipeCreateSerializer
+
+    def get_permissions(self):
+        if self.action == 'list' or self.action == 'retrieve':
+            self.permission_classes = [AllowAny]
+        elif self.action == 'create':
+            self.permission_classes = [IsAuthenticated]
+        return super().get_permissions()
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
